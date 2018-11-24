f01aaea45401d8c0291b765ff10575989d9088e7
Yatoub42 <paul.collin42@gmail.com>
2018-11-24 18:55:49 +0100

    Ajout messages personnalisés
    Ajout des messages personnalisés en cas de réussite ou d'échec critique et commentaires de code

--- dicecord.js
+++ dicecord.js
@@ -1,11 +1,49 @@
 // Libraries
 const Discord = require('discord.js'); //npm install discord.js --save
-const Auth = require('./Include/auth.priv.json');
 var Chance = require('chance');//npm install chance --save
+var Fs = require('fs');
+// Fichiers JSON
+const Auth = require('./Include/auth.priv.json');
+var Critpos = Fs.readFileSync("./Include/critpos.json");
+var Critneg= Fs.readFileSync("./Include/critneg.json");
+// Parser les fichiers JSON
+var contentCritpos = JSON.parse(Critpos);
+var contentCritneg = JSON.parse(Critneg);
 // Instanciations
 var chance = new Chance();
 const client = new Discord.Client();
 
+// Fonction de génération de réponse personnalisé en cas de réussite critique
+function reussiteCritique(){
+  var aleat = gen(1,2);
+  console.log('aleat reussite critique='+aleat)
+  switch (aleat) {
+    case 1:
+      message = contentCritpos.key1;
+      break;
+    case 2:
+      message = contentCritpos.key2;
+      break;
+  }
+  return message
+}
+
+// Fonction de génération de réponse personnalisé en cas d'échec critique
+function echecCritique(){
+  var aleat = gen(1,2);
+  console.log('aleat echec critique='+aleat)
+  switch (aleat) {
+    case 1:
+      message = contentCritneg.key1;
+      break;
+    case 2:
+      message = contentCritneg.key2;
+      break;
+  }
+  return message
+}
+
+// Fonction de génération de jet aléatoire
 function gen(min,max) {
     var instance = new Chance();
     var rng = instance.timestamp();
@@ -14,93 +52,74 @@ function gen(min,max) {
 	return result
 }
 
+// Validation de la connexion
 client.on('ready', () => {
 	client.user.setAvatar('./Include/avatar.png');
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+// Commandes et réponses
 client.on('message', msg => {
     switch (msg.content) {
         case '!1d100':
             var dice='1d100';
-            console.log(dice+' asked');
             var resultat = gen(0,100)
-            msg.reply(dice+'='+resultat);
-            console.log(resultat+' provided');
+            if (resultat >= 95) {
+              msg.reply(dice+'='+resultat+'\n'+echecCritique())
+            } else if (resultat && (resultat <= 5 || resultat == 42)) {
+              msg.reply(dice+'='+resultat+'\n'+reussiteCritique());
+            } else {
+              msg.reply(dice+'='+resultat);
+            }
             count++;
-            console.log(count+'lancés faits');
             break;
         case '!1d10':
             var dice='1d10';
-            console.log(dice+' asked');
             var resultat = gen(0,10)
             msg.reply(dice+'='+resultat);
-            console.log(resultat+' provided');
             count++;
-            console.log(count+'lancés faits');
             break;
         case '!1d12':
             var dice='1d12';
-            console.log(dice+' asked');
             var resultat = gen(1,12)
-            msg.reply(dice+'='+resultat);
-            console.log(resultat+' provided');
+            msg.reply(dice+'='+resultat)
             count++;
-            console.log(count+'lancés faits');
             break;
         case '!1d2':
             var dice='1d2';
-            console.log(dice+' asked');
             var resultat = gen(1,2)
             msg.reply(dice+'='+resultat);
-            console.log(resultat+' provided');
             count++;
-            console.log(count+'lancés faits');
             break;
         case '!1d3':
             var dice='1d3';
-            console.log(dice+' asked');
             var resultat = gen(1,3)
             msg.reply(dice+'='+resultat);
-            console.log(resultat+' provided');
             count++;
-            console.log(count+'lancés faits');
             break;
         case '!1d4':
             var dice='1d4';
-            console.log(dice+' asked');
             var resultat = gen(1,4)
             msg.reply(dice+'='+resultat);
-            console.log(resultat+' provided');
             count++;
-            console.log(count+'lancés faits');
             break;
         case '!1d6':
             var dice='1d6';
-            console.log(dice+' asked');
             var resultat = gen(1,6)
             msg.reply(dice+'='+resultat);
-            console.log(resultat+' provided');
             count++;
-            console.log(count+'lancés faits');
             break;
         case '!1d8':
             var dice='1d8';
-            console.log(dice+' asked');
             var resultat = gen(1,8)
             msg.reply(dice+'='+resultat);
-            console.log(resultat+' provided');
             count++;
-            console.log(count+'lancés faits');
             break;
         case '!1d20':
             var dice='1d20';
-            console.log(dice+' asked');
             var resultat = gen(1,20)
             msg.reply(dice+'='+resultat);
-            console.log(resultat+' provided');
             count++;
-            console.log(count+'lancés faits');
             break;
 		/*case '!debug':
             var dice='DEBUG'
@@ -119,8 +138,10 @@ client.on('message', msg => {
 		case '!stat':
 			msg.reply('Il y a eu '+count+' lancés depuis mon démarrage');
     }
+    console.log(count+' lancés faits');
 });
 
+// connexion du bot aux salons
 client.login(Auth.token);
 client.on('error', console.error);
 var count= 0
