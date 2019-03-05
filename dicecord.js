06bcd1bf3b10d2c8bdedae7ab69d25fec6801ff2
Yatoub42 <paul.collin42@gmail.com>
2019-03-05 17:08:30 +0100

    Update dicecord.js

--- dicecord.js
+++ dicecord.js
@@ -5,7 +5,7 @@ const db = require('./lib/bdd.js');
 //officielles
 const Discord = require('discord.js'); //npm install discord.js --save
 var program = require('commander'); //npm install commander --save
-const SqlString = require('sqlstring');
+const SqlString = require('sqlstring-sqlite');
 // Fichiers
 const Auth = require('./include/auth.priv.json');
 // Instanciations
