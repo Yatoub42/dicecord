e55750fa2c9233dc1a8b87ebbd03889280106868
Yatoub42 <17857978+Yatoub42@users.noreply.github.com>
2020-11-13 03:44:18 +0100

    test

--- dicecord.js
+++ dicecord.js
@@ -3,19 +3,21 @@ const message = require("./lib/message.js");
 const Discord = require("discord.js");
 const instance = require("commander"); 
 const Include = require("./include/resource.priv.json");
-const Client = new Discord.Client(/*{autoReconnect:true}*/);
+const client = new Discord.client(/*{autoReconnect:true}*/);
 
 const reSpec = /\W|_/g;
 const prefix = Include.prefix;
 
-Client.on("ready", () => {
-  Client.user.setActivity("vous juger !");
+client.on("ready", () => {
+  client.user.setActivity("vous juger !");
   console.info("Bot Connected");
 });
 
 db.createdb();
 
-Client.on("message", (msg) => {
+client.on('error', console.error); // Afficher les erreurs
+
+client.on("message", (msg) => {
     let ServerName = msg.guild.name.replace(reSpec,"");
     db.createTable(ServerName);
     if (msg.author.bot) {
@@ -41,48 +43,49 @@ instance
   .parse(process.argv);
 
 /*if (instance.niven) {
-    Client.login(Include.token1);
-    Client.on("error", (e) => console.error(e));
+    client.login(Include.token1);
+    //client.on("error", (e) => console.error(e));
 } else if (instance.nyria) {
-    Client.login(Include.token2);
-    Client.on("error", (e) => console.error(e));
+    client.login(Include.token2);
+    //client.on("error", (e) => console.error(e));
 } else if (instance.test) {
-    Client.login(Include.tokenTest);
-    Client.on("error", (e) => console.error(e));
+    client.login(Include.tokenTest);
+    //client.on("error", (e) => console.error(e));
 } else if (instance.chernobyl) {
-    Client.login(Include.token4);
-    Client.on("error", (e) => console.error(e));
+    client.login(Include.token4);
+    //client.on("error", (e) => console.error(e));
 } else if (instance.tanai) {
-    Client.login(Include.token5);
-    Client.on("error", (e) => console.error(e));
+    client.login(Include.token5);
+    //client.on("error", (e) => console.error(e));
 }*/
 
 switch (instance) {
     case instance.niven:
-        Client.login(Include.token1);
-        Client.on("error", (e) => console.error(e));
+        client.login(Include.token1);
+        //client.on("error", (e) => console.error(e));
     case instance.nyria:
-        Client.login(Include.token2);
-        Client.on("error", (e) => console.error(e));
+        client.login(Include.token2);
+        //client.on("error", (e) => console.error(e));
     case instance.test:
-        Client.login(Include.tokenTest);
-        console.log(Client.fetchApplication());
-        Client.on("error", (e) => console.error(e));
+        client.login(Include.tokenTest);
+        console.log(client.fetchApplication());
+        //client.on("error", (e) => console.error(e));
     case instance.chernobyl:
-        Client.login(Include.token4);
-        Client.on("error", (e) => console.error(e));
+        client.login(Include.token4);
+        //client.on("error", (e) => console.error(e));
     case instance.tanai:
-        Client.login(Include.token5);
-        Client.on("error", (e) => console.error(e));
+        client.login(Include.token5);
+        //client.on("error", (e) => console.error(e));
       break;
     default:
       console.log("/!\\ TOKEN NOT FOUND /!\\");
+      //client.on("error", (e) => console.error(e));
       /*console.log("Token list :\n"
       +"Include.token1="+Include.token1+"\n"
       +"Include.token2="+Include.token2+"\n"
       +"Include.tokenTest="+Include.tokenTest+"\n"
       +"Include.token4="+Include.token4+"\n"
       +"Include.token5="+Include.token5);*/
-      Client.destroy();
+      client.destroy();
   }
   
